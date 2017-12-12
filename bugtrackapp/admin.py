1dbeaa910ad3acca6a21f501bebc21cf2dfe5a84
santhosh <santhoshcusat17@gmail.com>
2017-12-12 22:35:47 +0530

    idea file added in gitignore app modified

--- bugtrackapp/admin.py
+++ bugtrackapp/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
 # Register your models here.
+from bugtrackapp.models import Bugtrack
+from django.utils.html import format_html
+from django.contrib.auth.models import User
+
+
+
+
+@admin.register(Bugtrack)
+class BugtrackAdmin(admin.ModelAdmin):
+    list_display = ['assigned_to','title','created_on','priority','current_status','final_status','points','img']
+    list_filter = ( 'priority','created_on',)
+
+    def img(self,obj):
+        return format_html('<img src="{}" width=50,height=40 alt="*"/>'.format(obj.upload_image.url))
+    img.allow_tags = True
+
+    def get_queryset(self, request):
+        qs = super(BugtrackAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(assigned_to=request.user)
+
+    def get_actions(self, request):
+        actions = super(BugtrackAdmin, self).get_actions(request)
+        if request.user.is_superuser:
+            return actions
+        else:
+            del actions['delete_selected']
+
+    def get_readonly_fields(self, request, obj=None):
+        # readonly_fields = ('admission', 'courses',)
+        if request.user.is_superuser:
+            return self.readonly_fields
+
+        # readonly_fields = ('title','description','assigned_to','created_on','priority','upload_image',
+        # 'reference','final_status','points',)
+
+        return self.readonly_fields + ('title','description','assigned_to','created_on','priority','upload_image','reference','final_status','points',)
--- bugtrackapp/models.py
+++ bugtrackapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+# from tinymce.models import HTMLField
+
+# from ckeditor.fields import RichTextField
+
+priority_choices = (('L','LOW'),('M','MEDIUM'),('H','HIGH'),)
+current_status_choice= (('N','NotCompleted'),('O','Onprocess'),('C','Completed'),)
+
+class Bugtrack(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    assigned_to = models.ForeignKey(User)
+    created_on = models.DateTimeField(auto_now_add=True, blank=True)
+    priority = models.CharField(max_length=2,choices=priority_choices)
+    current_status = models.CharField(max_length=2,default='N',choices=current_status_choice)
+    upload_image = models.ImageField(upload_to='butrackimg/',default='null')
+    reference = models.CharField(max_length=40,default="Not Available")
+    final_status = models.BooleanField(default=False)
+    points = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
