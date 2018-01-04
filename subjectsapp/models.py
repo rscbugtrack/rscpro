e3cc34b0c3db97778f0c5768e9728adce928ca6f
santhosh <santhoshcusat17@gmail.com>
2018-01-04 22:57:47 +0530

    minor changes...

--- subjectsapp/models.py
+++ subjectsapp/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 # Create your models here.
 
 class TechType(models.Model):
     subject_name = models.CharField(max_length=30, help_text='Add the technology type ..like python , jave and mysql etc..')
     status = models.BooleanField(default=False)
     user = models.ForeignKey(User,default='null', null=True)
+
+    def get_absolute_url(self):
+        return reverse('subjectapp:editsubject', kwargs={'pk': self.pk})
