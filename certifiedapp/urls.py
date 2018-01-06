febf481074f7529fdad365d683b14fcc8bbdc61c
rscbugtrack <you@example.com>
2018-01-06 13:53:11 +0530

    signup align, logout and dashboard design

--- certifiedapp/urls.py
+++ certifiedapp/urls.py
@@ -5,8 +5,9 @@ from certifiedapp import views as core_views
 
 urlpatterns =[
     # url(r'^login/$', auth_views.login,  name='login'),
-    # url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/login/'}),
     url(r'^signup/$', core_views.signup, name='signup'),
+
     url(r'^userdashbord/$', core_views.userdashbord, name='userdashbord'),
 
 ]
