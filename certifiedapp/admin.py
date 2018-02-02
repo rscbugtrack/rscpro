a27df1a8521b0783b46ba1004d861424850235f5
santhosh432 <y.santhosh32kumar@gmail.com>
2018-02-02 13:48:36 +0530

    newuser proxy model created

--- certifiedapp/admin.py
+++ certifiedapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Newuser
+
+@admin.register(Newuser)
+class NewUserAdmin(admin.ModelAdmin):
+    list_display = ['username','email','first_name', 'last_name','is_staff','is_superuser']
+    fields = ('username','email','first_name','last_name',)
--- certifiedapp/models.py
+++ certifiedapp/models.py
@@ -6,6 +6,16 @@ class Testlist(models.Model):
     tst_name = models.CharField(max_length=50,default="coming soon")
     
 
-   
+class Newuser(User):
+    object = UserManager()
+
+    class Meta:
+        proxy = True
+        verbose_name = "New user model"
+        verbose_name_plural = "New user models"
+
+    def __str__(self):
+        return 'Username:{}, email:{},lastname: {}'.format(self.username,self.email,self.last_name)
+
 
 
