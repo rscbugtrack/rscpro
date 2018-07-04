317328071ecb72a1ff5ffbc89c87d9632400bc9b
rscbugtrack <rscbugtrack@gmail.com>
2018-07-04 05:33:44 +0000

    home and other changes

--- bugtrackapp/admin.py
+++ bugtrackapp/admin.py
@@ -1,10 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
-from bugtrackapp.models import Bugtrack, Comments
+from bugtrackapp.models import *
 from django.utils.html import format_html
 from django.contrib.auth.models import User
 
+
+class ModelAdmin(admin.ModelAdmin):
+    """
+    Abstract admin models for populating columns updated_by and created_by
+    """
+    exclude = ('updated_by', 'created_by',)
+
+    def save_form(self, request, form, change):
+        obj = super(ModelAdmin, self).save_form(request, form, change)
+
+        if hasattr(obj, 'updated_by'):
+            obj.updated_by = str(request.user)
+
+        if hasattr(obj, 'created_by') and not obj.created_by:
+            obj.created_by = str(request.user)
+
+        return obj
+
+class SysConfigAdmin(ModelAdmin):
+    list_display = ('name', 'value', 'description',)
+    search_fields = ('name', 'value', 'description',)
+    date_hierarchy = 'created_on'
+
+
+admin.site.register(SysConfig, SysConfigAdmin)
 
 @admin.register(Comments)
 class CommentsAdmin(admin.ModelAdmin):
--- bugtrackapp/views.py
+++ bugtrackapp/views.py
@@ -5,6 +5,37 @@ from bugtrackapp.forms import FeedbackForm
 from bugtrackapp.models import Comments
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+
+#from __future__ import unicode_literals
+
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.core.exceptions import ImproperlyConfigured
+
+from .models import SysConfig
+
+
+def send_mail(subject, message, recipients, is_html):
+    """
+    Sends email using predefined System Configs and Django settings
+    """
+    sys_configs = SysConfig.get_configs()
+
+    if 'SITE_NAME' not in sys_configs or 'DOMAIN' not in sys_configs:
+        raise ImproperlyConfigured('SITE_NAME and DOMAIN not found. It should be defined in System Configurations')
+
+    if not hasattr(settings, 'EMAIL_HOST') or not hasattr(settings, 'EMAIL_PORT'):
+        raise ImproperlyConfigured('Email server configurations not found in django settings.py.')
+
+    from_email = '%s <noreply@%s>' % (sys_configs['SITE_NAME'], sys_configs['DOMAIN'])
+    msg = EmailMessage(subject, message, from_email, recipients)
+    if is_html:
+        msg.content_subtype = "html"
+    msg.send()
+
+
+
+
 def blog_id(request,post_id):
     blog_obj = Post.objects.get(id=post_id)
     body_contain = blog_obj.body.split("^")
@@ -14,18 +45,18 @@ def blog_id(request,post_id):
 def rschome(request):
 
 
-    blog_obj = Post.objects.all().order_by('-id')[:15]
-    page = request.GET.get('page', 1)
-    cmnt_objs = Comments.objects.all().order_by('-id')[:3]
-    paginator = Paginator(blog_obj, 5)
-    try:
-        blog_page = paginator.page(page)
-    except PageNotAnInteger:
-        blog_page = paginator.page(1)
-    except EmptyPage:
-        blog_page = paginator.page(paginator.num_pages)
+    #blog_obj = Post.objects.all().order_by('-id')[:15]
+    #page = request.GET.get('page', 1)
+    #cmnt_objs = Comments.objects.all().order_by('-id')[:3]
+    #paginator = Paginator(blog_obj, 5)
+    #try:
+    #    blog_page = paginator.page(page)
+    #except PageNotAnInteger:
+    #    blog_page = paginator.page(1)
+    #except EmptyPage:
+    #    blog_page = paginator.page(paginator.num_pages)
 
-    context = {'blog_obj':blog_obj,'blog_page':blog_page,'comments':cmnt_objs}
+    context = {}#'blog_obj':blog_obj,'blog_page':blog_page,'comments':cmnt_objs}
     return render(request,'home.html',context)
 
 def comments(request):
